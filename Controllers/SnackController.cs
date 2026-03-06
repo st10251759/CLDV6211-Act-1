aa092df57d1768aec81ba081d6f717dca0d905e6
Cameron Chetty <ST10251759@vcconnect.edu.za>
2026-03-06 09:36:29 +0200

    Add project files.

---
+++ Controllers/SnackController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SnackMVCApp.Models;
+
+namespace SnackMVCApp.Controllers
+{
+    public class SnacksController : Controller
+    {
+        private static List<Snack> snacks = new List<Snack>()
+        {
+            new Snack{
+                Id = 1,
+                Name = "TopDeck",
+                Brand = "Cadbury",
+                Type = SnackType.Chocolate,
+                Description = "Layers of white and milk chocolate",
+                Rating = 5,
+                Price = 18
+            },
+            new Snack{
+                Id = 2,
+                Name = "Bubble Tea",
+                Brand = "KungFu Tea",
+                Type = SnackType.Drink,
+                Description = "Sweet tea with tapioca pearls",
+                Rating = 4,
+                Price = 35
+            }
+        };
+
+        // READ
+        public IActionResult Index()
+        {
+            return View(snacks);
+        }
+
+        // DETAILS
+        public IActionResult Details(int id)
+        {
+            var snack = snacks.FirstOrDefault(s => s.Id == id);
+
+            if (snack == null)
+                return NotFound();
+
+            return View(snack);
+        }
+
+        // CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Snack snack)
+        {
+            if (ModelState.IsValid)
+            {
+                snack.Id = snacks.Max(s => s.Id) + 1;
+                snacks.Add(snack);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(snack);
+        }
+
+        // EDIT
+        public IActionResult Edit(int id)
+        {
+            var snack = snacks.FirstOrDefault(s => s.Id == id);
+
+            if (snack == null)
+                return NotFound();
+
+            return View(snack);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Snack snack)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingSnack = snacks.FirstOrDefault(s => s.Id == snack.Id);
+
+                if (existingSnack == null)
+                    return NotFound();
+
+                existingSnack.Name = snack.Name;
+                existingSnack.Brand = snack.Brand;
+                existingSnack.Type = snack.Type;
+                existingSnack.Description = snack.Description;
+                existingSnack.Rating = snack.Rating;
+                existingSnack.Price = snack.Price;
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(snack);
+        }
+
+        // DELETE
+        public IActionResult Delete(int id)
+        {
+            var snack = snacks.FirstOrDefault(s => s.Id == id);
+
+            if (snack == null)
+                return NotFound();
+
+            return View(snack);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var snack = snacks.FirstOrDefault(s => s.Id == id);
+
+            snacks.Remove(snack);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Snack.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SnackMVCApp.Models
+{
+    public enum SnackType
+    {
+        Chocolate,
+        Drink,
+        Chips,
+        ProteinSnack,
+        FastFood,
+        Candy
+    }
+
+    public class Snack
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Brand { get; set; }
+
+        [Required]
+        public SnackType Type { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        [Range(0, 1000)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+    }
+}
